44772cc22b88b96562a114f1b2b9e90813398025
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-09-05 22:30:59 +0800

    change:inference

--- scripts/inference.py
+++ scripts/inference.py
@@ -3,13 +3,17 @@ import cv2
 import argparse
 import numpy as np
 import onnxruntime
-from utils import resize_image_to_kb, hex_to_rgb
-from hivision.creator.face_judgement_align import IDphotos_create
-from hivision.creator.vision import add_background
-from hivision.creator.layoutCreate import generate_layout_photo, generate_layout_image
-
+from hivision.error import FaceError
+from hivision.utils import hex_to_rgb, resize_image_to_kb, add_background
+from hivision import IDCreator
+from hivision.creator.layout_calculator import (
+    generate_layout_photo,
+    generate_layout_image,
+)
 
 parser = argparse.ArgumentParser(description="HivisionIDPhotos 证件照制作推理程序。")
+
+creator = IDCreator()
 
 parser.add_argument(
     "-t",
@@ -43,40 +47,18 @@ if args.type == "idphoto":
 
     # 将字符串转为元组
     size = (int(args.height), int(args.width))
-
-    (
-        result_image_hd,
-        result_image_standard,
-        typography_arr,
-        typography_rotate,
-        _,
-        _,
-        _,
-        _,
-        status,
-    ) = IDphotos_create(
-        input_image,
-        size=size,
-        align=False,
-        beauty=False,
-        fd68=None,
-        human_sess=sess,
-        IS_DEBUG=False,
-    )
-
-    # 如果检测到人脸数量不等于 1（照片无人脸 or 多人脸）
-    if status == 0:
+    try:
+        result = creator(input_image, size=size)
+    except FaceError:
         print("人脸数量不等于 1，请上传单张人脸的图像。")
-
-    # 如果检测到人脸数量等于 1, 则返回标准证和高清照结果（png 4 通道图像）
     else:
         # 保存标准照
-        cv2.imwrite(args.output_image_dir, result_image_standard)
+        cv2.imwrite(args.output_image_dir, result.standard)
 
         # 保存高清照
         file_name, file_extension = os.path.splitext(args.output_image_dir)
         new_file_name = file_name + "_hd" + file_extension
-        cv2.imwrite(new_file_name, result_image_hd)
+        cv2.imwrite(new_file_name, result.hd)
 
 # 如果模式是添加背景
 elif args.type == "add_background":
